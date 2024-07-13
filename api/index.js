9cf41bbaf213a969e12af06bdecfedf68843fdb8
vanthinh01052001 <vanthinh01052001@gmail.com>
2024-07-13 15:29:19 +0700

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.route.js';
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -12,8 +13,10 @@ mongoose
     console.log(error);
   });
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,6 +1,4 @@
 import mongoose from "mongoose";
-import { type } from "os";
-
 const userSchema = new mongoose.Schema(
   {
     username: {
