4893dae3e82fac61e4fd985b89fc6db4fd93a244
vanthinh01052001 <vanthinh01052001@gmail.com>
2024-07-13 13:35:58 +0700

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
-import express from 'express'
-const app = express()
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000');
-})
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is connected.");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+const app = express();
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
