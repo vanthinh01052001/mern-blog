c5cfb2295fb4f86bed169ac59ef2211471178ed6
vanthinh01052001 <vanthinh01052001@gmail.com>
2024-07-17 15:58:09 +0700

    add like functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Alert, Button, Textarea } from "flowbite-react";
 import Comment from "./Comment";
 
 export default function CommentSection(props) {
   const { postId } = props;
+  const navigate = useNavigate();
   const [comment, setComment] = useState("");
   const [commentError, setCommentError] = useState(null);
   const { currentUser } = useSelector((state) => state.user);
-  const [comments, setComments] = useState([])
-  console.log(comments)
+  const [comments, setComments] = useState([]);
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (comment.length > 200) {
@@ -32,26 +32,53 @@ export default function CommentSection(props) {
       if (res.ok) {
         setComment("");
         setCommentError(null);
-        setComments([data, ...comments])
+        setComments([data, ...comments]);
       }
     } catch (error) {
       setCommentError(error.message);
     }
   };
-  useEffect(()=>{
-    const getComments = async () =>{
+  useEffect(() => {
+    const getComments = async () => {
       try {
-        const res = await fetch(`/api/comment/getPostComments/${postId}`)
-        if(res.ok) {
+        const res = await fetch(`/api/comment/getPostComments/${postId}`);
+        if (res.ok) {
           const data = await res.json();
           setComments(data);
         }
       } catch (error) {
-        console.log(error.message)
+        console.log(error.message);
       }
-    }
+    };
     getComments();
-  },[postId])
+  }, [postId]);
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate("/sign-in");
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: "put",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -114,8 +141,8 @@ export default function CommentSection(props) {
               <p>{comments.length}</p>
             </div>
           </div>
-          {comments.map(comment => (
-            <Comment key={comment._id} comment={comment} />
+          {comments.map((comment) => (
+            <Comment key={comment._id} comment={comment} onLike={handleLike} />
           ))}
         </>
       )}
