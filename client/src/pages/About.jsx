0b79047f44701c55391c28cb379906bde44a65ac
vanthinh01052001 <vanthinh01052001@gmail.com>
2024-07-18 12:45:18 +0700

    complete about and project page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,25 @@
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex flex-col items-center">
+      <h1 className="text-3xl font font-semibold text-center my-7">
+        About vanthinh'blog
+      </h1>
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="text-md text-gray-500 flex flex-col gap-6">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
+            tempore autem, dolorem unde, sit eius deleniti velit vel quisquam
+            nobis optio fuga similique eum aut sint libero deserunt? Ipsa,
+            accusantium.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+            illo quae sint id delectus suscipit aliquid. Unde illo
+            exercitationem tempore a architecto et recusandae molestias, eum
+            quod, deserunt ad cumque.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,13 @@
 export default function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="min-h-screen max-w-2xl mx-auto flex items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+        cupiditate animi rem illum, vitae eveniet minus! Corporis natus, vel
+        nobis sunt reprehenderit voluptates aspernatur consequatur quam eveniet
+        iusto, porro enim.
+      </p>
+    </div>
+  );
 }
